538f967d9cbaf7b52c3f01f6c23ea177461f596e
Ivorfeng <ivorfeng@icloud.com>
2016-07-14 07:11:04 +0800

    add DotCom Sample

---
+++ DotCom/DotCom.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class DotCom {
+
+	private ArrayList<String> locationCells;
+	private String name;
+	
+	public void setLocationCells(ArrayList<String> locs) {
+		locationCells = locs;
+	}
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public String checkYourself(String stringGuess) {
+		
+		String result = "miss";
+		int index = locationCells.indexOf(stringGuess);
+		
+		if (index >= 0){
+			locationCells.remove(index);
+			
+			if (locationCells.isEmpty()){
+				result = "kill";
+			}else {
+				result = "hit";
+			}
+		}
+		
+		System.out.println(result);
+		
+		return result;
+	}
+
+}
