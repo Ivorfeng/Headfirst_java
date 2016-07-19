2bbb96908e99edfe362d1b9204d9543edc7cef56
Ivorfeng <ivorfeng@icloud.com>
2016-07-20 07:20:10 +0800

    2 button listener

--- SimpleGui/MyDrawPanel.java
+++ SimpleGui/MyDrawPanel.java
@@ -3,8 +3,20 @@ import javax.swing.*;
 
 public class MyDrawPanel extends JPanel {
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.orange);
+		Graphics2D g2d = (Graphics2D)g;
 		
-		g.fillRect(20, 50, 100, 100);
+		int red = (int) (Math.random() * 255);
+		int green = (int)(Math.random() * 255);
+		int blue = (int)(Math.random() * 255);
+		Color startColor = new Color(red, green, blue);
+		
+		red = (int) (Math.random() * 255);
+		green = (int)(Math.random() * 255);
+		blue = (int)(Math.random() * 255);
+		Color endColor = new Color(red, green, blue);
+		
+		GradientPaint gradient = new GradientPaint(70, 70, startColor, 150,150, endColor);
+		g2d.setPaint(gradient);
+		g2d.fillOval(50, 70, 100, 100);
 	}
 }
